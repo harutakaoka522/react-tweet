bc2be43c3f31ec123b481dfdd6395ea11f247825
hogehoge <AAAgmail.com>
2020-06-28 20:29:07 +0900

    tweet系

--- main.jsx
+++ main.jsx
@@ -1,11 +1,30 @@
-function App(){
+function App() {
+    const [tweets, setTweets] = React.useState([
+      {
+        id: 0,
+        icon: '🌽',
+        displayName: 'もろこし太郎',
+        accountName: 'morokoshi',
+        content: '今日も1日もろこしがうまい'
+      },
+      {
+        id: 1,
+        icon: '🦐',
+        displayName: 'エビデンス',
+        accountName: 'evidence',
+        content: 'かにみそたべたい'
+      }
+    ]);
+  
+    const addTweet = React.useCallback((tweet) => setTweets((prev) => [tweet, ...prev]), [setTweets]);
+  
     return (
-        <div>
-            <Tweet content="hello"/>
-            <Tweet content="react"/>
-        </div>
+      <div>
+        <TweetInput addTweet={addTweet}/>
+        <Timeline tweets={tweets}/>
+      </div>
     );
-}
-
-const target = document.querySelector('#app');
-ReactDOM.render(<App/>,target);
+  }
+  
+  const target = document.querySelector('#app');
+  ReactDOM.render(<App/>, target);
